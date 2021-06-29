df36d44536fd444a44fb819eeaed1cdd515b88df
abhilash-naik <abhilash.naik@thermofisher.com>
2021-06-29 07:41:11 +0530

    Added header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.styles.scss';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <Link to='/' className='logo-container'>
+                <Logo className='logo'/>
+            </Link>
+            <div className='options'>
+                <Link to='/shop' className='option' >
+                    SHOP
+                </Link>
+                <Link to='/shop' className='option' >
+                    CONTACT
+                </Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
