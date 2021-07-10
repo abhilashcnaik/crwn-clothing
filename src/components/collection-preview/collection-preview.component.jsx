0ae81aede3978a37c83cb56c88b671ffe5e46349
abhilash-naik <abhilash.naik@thermofisher.com>
2021-07-11 04:13:36 +0530

    updated cart with quantity

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, index) => index < 4).map(({ id, ...otherProps }) => (
-                    <CollectionItem key={id} {...otherProps} />
+                items.filter((item, index) => index < 4).map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
