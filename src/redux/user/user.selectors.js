42108db29646d8eea38cbec0ae4e2ce516c281f5
abhilash-naik <abhilash.naik@thermofisher.com>
2021-07-13 13:08:35 +0530

    Added shop reducer, directory reducer, persistor to store the state in local storage
    Added shop reducer, directory reducer, persistor to store the state in local storage

--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -1,3 +1,4 @@
+// selectors are used to maintain the state so that the component is not rerendered when state does not change
 import { createSelector } from "reselect";
 
 const selectUser = state => state.user;
