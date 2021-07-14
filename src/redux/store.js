740ee123d06bc0dffafabe825cc3ff419eac40c3
naik-abhilash <abhilash.naik@gmail.com>
2021-07-14 14:41:19 +0530

    added logger middleware only for development environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import logger from 'redux-logger';
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+console.log('environment ',process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
