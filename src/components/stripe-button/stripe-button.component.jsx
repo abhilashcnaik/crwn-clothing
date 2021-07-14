efb20d205619115122c46ac4a569591c623c23b9
abhilash-naik <abhilash.naik@thermofisher.com>
2021-07-14 11:39:24 +0530

    Updating Stripe integration and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JCx0wSEfxtFUFAPfOsiLlnt4HLIFJNOWdeKDE4GNzlZ3YbEQhrWPB2kDkIKmowPxRMsFc0Kd4J5oBbLRjwj2UPk00Z0XJJkvc';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crown Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+}
+
+export default StripeCheckoutButton;
+
+
