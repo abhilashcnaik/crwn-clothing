69891a4b249bd468a754461999aa0e253e6e343f
abhilash-naik <abhilash.naik@thermofisher.com>
2021-07-14 06:54:20 +0530

    Created Collections overview, Collection, updated shop selectors

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+import { memoize } from "lodash";
 
 const selectShop = state => state.shop;
 
@@ -6,3 +7,18 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollectionForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = memoize((collectionUrlParam) => {
+    console.log('collectionUrlParam ', collectionUrlParam);
+    return (
+        createSelector(
+            [selectCollections],
+            collections => collections[collectionUrlParam]
+        )
+    )
+})
