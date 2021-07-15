31944f60591ab0e36f78965d2e22caddb0196566
abhilash-naik <abhilash.naik@thermofisher.com>
2021-07-15 08:15:09 +0530

    removed unused variable

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,7 +24,6 @@ class ShopPage extends Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
-        const isLoading = true;
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
